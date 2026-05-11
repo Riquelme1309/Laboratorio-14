5b37e1c40e1aef2c992e0ae9109b1e0aa85fa7ca
SAMSUNG <SAMSUNG@LAPTOP-9FD33DQF>
2026-05-11 08:51:31 -0600

    Agregar archivos de proyecto.

---
+++ Models/Albun.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio__14.Models
+{
+    public class Albun
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+        public DateTime FechaPublicacion { get; set; }
+        public List<Cancion> Canciones { get; set; } = new();
+    }
+}
---
+++ Models/Cancion.cs
@@ -0,0 +1,10 @@
+﻿namespace Laboratorio__14.Models
+{
+    public class Cancion
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Artista { get; set; } = string.Empty;
+        public TimeSpan Duracion { get; set; }
+    }
+}
